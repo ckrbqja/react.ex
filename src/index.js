807a9e2d90b4d294c6c129a3ea430046b77e62ce
ckrbqja <67684166+ckrbqja@users.noreply.github.com>
2021-07-18 09:17:51 +0900

    gd

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import "@fortawesome/fontawesome-free/js/all.js"
 import App from "./app"
-import SimpleHabit from "./components/habitAdd"
 
 ReactDOM.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <SimpleHabit />
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 )
